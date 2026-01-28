d53b78c607e9bca477551ddeb62777a4dcb76e19
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-01-28 10:40:00 +0100

    literals vs new

--- Text-Date-Time-Numeric Objects/src/main/java/com/dalal/strings/Test.java
+++ Text-Date-Time-Numeric Objects/src/main/java/com/dalal/strings/Test.java
@@ -1,5 +1,30 @@
 package com.dalal.strings;
 
+import java.awt.*;
+
 public class Test {
+    public static void main(String[] args) {
+        //diff between string new and string literals
+
+        //because with new jvm create two differente object
+        String name = new String("youness");
+        String name2 = new String("youness");
+        System.out.println(name == name2); //false
+
+        // string literals objects stored in string pool object
+        String fruitName = "banana";
+        String fruitName2 = "banana";
+        System.out.println(fruitName == fruitName2);//true
+
+        // using intern methode to return the reference
+        String a = new String("abc");
+        String b = "abc";
+        System.out.println(a == b); //false
+
+        String c = a.intern();
+        System.out.println(b==c);//true
+
+
+    }
 
 }
