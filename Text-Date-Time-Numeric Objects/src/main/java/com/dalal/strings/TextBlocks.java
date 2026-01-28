b83423658566358ab32cf4cf7181c8d47729e055
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-01-28 13:46:00 +0100

    textblock & escape sequences

--- Text-Date-Time-Numeric Objects/src/main/java/com/dalal/strings/TextBlocks.java
+++ Text-Date-Time-Numeric Objects/src/main/java/com/dalal/strings/TextBlocks.java
@@ -16,5 +16,41 @@ public class TextBlocks {
         System.out.println(textBlock);
 
         //Text block = multiline string literal
+        //toujours java se base sur les """ dans la fin pour creer les espaces
+
+        System.out.println("*".repeat(20));
+        String text = """
+                je suis youness dalal
+                """;
+        System.out.println(text);//y'as pas d'espace avant le je suis
+        //mais
+        text = """
+             je suis dalal youness
+                 je serai un ingenieur incha allah
+           """;
+        System.out.println("*".repeat(20));
+        //on observe que je suis mka7za ela akhir """ mabdatch meaha f nfs lbadya dkchi elach kayn espace
+        System.out.println(text);
+
+        System.out.println("*".repeat(20));
+        //mais!!!!
+        text = """
+           je suis dalal youness
+                 je serai un ingenieur incha allah
+           """;
+        System.out.println(text);
+
+        //conclusion : chaque ligne devrait etre au meme debut que les derniers """ si non
+        // la jvm va la considerer comme un espace c'est pour ca on a pas le droit de faire """ """ directement
+        //\s si je veux ajouter un espace
+        //\si je veux par exemple eviter le multiline
+        //\""" pour ajouter un text contient les """
+
+        System.out.println("*".repeat(20));
+        String exemple = """
+                je suis\s\
+                dalal youness
+                """;
+        System.out.println(exemple); //pour eviter que les lignes collé je met un\s
     }
 }
