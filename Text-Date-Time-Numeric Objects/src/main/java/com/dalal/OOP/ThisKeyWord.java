3d61929fd737ab99979473dfab411de4bd456710
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-01-29 12:28:14 +0100

    oop

---
+++ Text-Date-Time-Numeric Objects/src/main/java/com/dalal/OOP/ThisKeyWord.java
@@ -0,0 +1,28 @@
+package com.dalal.OOP;
+
+public class ThisKeyWord
+{
+    public static void main(String[] args)
+    {
+        /*this c'est un reference implicite , katsifto la jvm par default f kola methode
+        * o hta fl constructeur c'est a dire mnin kandiro haka
+        *
+        * class Person {
+    String name;
+    int age;
+
+    Person(String name) {
+        // JVM داخليًا: Person(Person this, String name)
+        this.name = name;
+    }
+
+    Person(String name, int age) {
+        // JVM داخليًا: Person(Person this, String name, int age)
+        this(name); // تعيّط على constructor الأول
+        this.age = age;
+    }
+}
+        * */
+
+    }
+}
--- Text-Date-Time-Numeric Objects/src/main/java/com/dalal/format_and_parse/NumericValues.java
+++ Text-Date-Time-Numeric Objects/src/main/java/com/dalal/format_and_parse/NumericValues.java
@@ -1,4 +1,7 @@
 package com.dalal.format_and_parse;
 
 public class NumericValues {
+    public static void main(String[] args) {
+
+    }
 }
