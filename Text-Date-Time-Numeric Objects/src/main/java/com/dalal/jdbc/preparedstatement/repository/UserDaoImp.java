218bd309b9da6b5c6a17e271bab4f6fdfc8b2032
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-02-06 15:25:57 +0100

    using setObject

--- Text-Date-Time-Numeric Objects/src/main/java/com/dalal/jdbc/preparedstatement/repository/UserDaoImp.java
+++ Text-Date-Time-Numeric Objects/src/main/java/com/dalal/jdbc/preparedstatement/repository/UserDaoImp.java
@@ -6,6 +6,7 @@ import com.dalal.jdbc.simplestatement.connection.ConnectionSingleton;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Types;
 
 public class UserDaoImp implements UserDao{
     private Connection connection = ConnectionSingleton.getConnection();
@@ -18,8 +19,11 @@ public class UserDaoImp implements UserDao{
     public void create(User user) throws SQLException {
         String insertQuery = "insert into users (username,mot_de_passe) values (?,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-        preparedStatement.setString(1,user.getUsername());
-        preparedStatement.setString(2,user.getPassword());
+      /*preparedStatement.setString(1,user.getUsername());
+        preparedStatement.setString(2,user.getPassword());*/
+        //or
+        preparedStatement.setObject(1,user.getUsername(), Types.VARCHAR);
+        preparedStatement.setObject(2,user.getPassword(), Types.VARCHAR);
         preparedStatement.executeUpdate();
     }
 }
