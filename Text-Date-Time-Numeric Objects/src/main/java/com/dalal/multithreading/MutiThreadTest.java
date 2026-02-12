531f935650abb3010c12b52d570a2179cdb3a361
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-02-12 23:53:51 +0100

    thread with stack & heap

--- Text-Date-Time-Numeric Objects/src/main/java/com/dalal/multithreading/MutiThreadTest.java
+++ Text-Date-Time-Numeric Objects/src/main/java/com/dalal/multithreading/MutiThreadTest.java
@@ -3,6 +3,7 @@ package com.dalal.multithreading;
 public class MutiThreadTest implements Runnable {
     @Override
     public void run() {
+
         System.out.println("test runnable");
     }
 
@@ -19,6 +20,12 @@ public class MutiThreadTest implements Runnable {
 * cpu mnin kayqra man ram o kayktab fiha
 * o multhread howa kaywliw 3adna bzf dyal lmasarat
 *
+* mnin kangolo un thread kayhjaz lih system d'exploitation stack whd 3an tariq system call
+* dyal jvm c'est normale hit kola thread khashom ykhdmo f envi dyalo bodho
+* mea les methode o kda o hdchi kaykhali ykono bzf dyal compies mais lheap dkchi li partager
+* kaykon fih fhal object donc l aventage dyal heap anaho zone partagé:
+* howa state shared hadi hiya aham haja hit 3adna nfs lobjet
+* o zid kon kano les objects kol thread coipe dyalo f stack kon rah karita f ram
 *
 *
 * */
