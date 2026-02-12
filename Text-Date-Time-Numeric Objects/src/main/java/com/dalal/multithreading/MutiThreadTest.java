131839ed5ed6414e7ee072ca6658e8475889fad1
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-02-12 23:47:50 +0100

    single thread & multithread concept

--- Text-Date-Time-Numeric Objects/src/main/java/com/dalal/multithreading/MutiThreadTest.java
+++ Text-Date-Time-Numeric Objects/src/main/java/com/dalal/multithreading/MutiThreadTest.java
@@ -2,3 +2,13 @@ package com.dalal.multithreading;
 
 public class MutiThreadTest {
 }
+
+
+/*
+* thread c'est un chemin d'execution howa l masar dyal
+* cpu mnin kayqra man ram o kayktab fiha
+* o multhread howa kaywliw 3adna bzf dyal lmasarat
+*
+*
+*
+* */
